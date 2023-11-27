acf2525d2a91b9538b8d87dbe6e94b5645e8f4b3
Shan Weiqiang <43443481+shan-weiqiang@users.noreply.github.com>
2023-11-27 22:09:22 +0800

    Update packer-plugins.lua

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -46,7 +46,7 @@ return require("packer").startup(function(use)
 	use("nvim-treesitter/playground")
 
 	-- telescope
-	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.4", requires = { { "nvim-lua/plenary.nvim" } } })
 
 	-- formatter
 	use({ "sbdchd/neoformat" })
