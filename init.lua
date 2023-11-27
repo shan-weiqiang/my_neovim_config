5ab5e520bd9351b8c61de0e3d94461129932dd8c
Shan Weiqiang <43443481+shan-weiqiang@users.noreply.github.com>
2023-11-27 21:04:04 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 -- neovim version= 0.8.0
 
+-- [[use packer.nvim to manage all plugins,https://github.com/wbthomason/packer.nvim
+-- all plugins are installed at default search path: ~/.local/share/nvim/site/pack/...
+-- use :h packages to find more information about neovim packages.
+-- packer.nvim must be first installed manully. Other plugins are installed automatically by packer.nvim
+-- following line is the initialization of packer.nvim, which automatically loads all other plugins.]]
+require("packer-plugins")
+
 vim.o.number = true
 vim.o.smartindent = true
 vim.o.termguicolors = true
@@ -39,12 +46,6 @@ require("nvim-tree-setup")
 -- keybindings
 require("keybindings")
 
--- [[use packer.nvim to manage all plugins,https://github.com/wbthomason/packer.nvim
--- all plugins are installed at default search path: ~/.local/share/nvim/site/pack/...
--- use :h packages to find more information about neovim packages.
--- packer.nvim must be first installed manully. Other plugins are installed automatically by packer.nvim
--- following line is the initialization of packer.nvim, which automatically loads all other plugins.]]
-require("packer-plugins")
 
 -- lsp-config
 require("user.lsp")
